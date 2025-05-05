4e9e886ec85eb7090d8a6cbddf323f04d24785a0
tomaThomas <tomathomas@mailbox.org>
2025-05-05 19:27:45 +0200

    Add ExoPlayer player

--- app/src/main/java/code/name/monkey/retromusic/service/MusicService.kt
+++ app/src/main/java/code/name/monkey/retromusic/service/MusicService.kt
@@ -837,7 +837,7 @@ class MusicService : MediaBrowserServiceCompat(),
     fun prepareNextImpl() {
         try {
             val nextPosition = getNextPosition(false)
-            playbackManager.setNextDataSource(getSongAt(nextPosition).uri.toString())
+            playbackManager.setNextDataSource(getSongAt(nextPosition).uri)
             this.nextPosition = nextPosition
         } catch (ignored: Exception) {
         }
