42c96e196f3f0debea42075e7489e931dd378698
tomaThomas <tomathomas@mailbox.org>
2025-05-17 10:49:36 +0200

    Remove MultiPlayer and gapless playback option

--- app/src/main/java/code/name/monkey/retromusic/util/PreferenceUtil.kt
+++ app/src/main/java/code/name/monkey/retromusic/util/PreferenceUtil.kt
@@ -315,11 +315,6 @@ object PreferenceUtil {
             putBoolean(DESATURATED_COLOR, value)
         }
 
-    val isGapLessPlayback
-        get() = sharedPreferences.getBoolean(
-            GAP_LESS_PLAYBACK, false
-        )
-
     val isAdaptiveColor
         get() = sharedPreferences.getBoolean(
             ADAPTIVE_COLOR_APP, false
