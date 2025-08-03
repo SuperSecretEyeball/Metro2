d29a73d05cc409bbd1a68500e42e4b940044fa10
PhracturedBlue <rc2012@pblue.org>
2025-08-02 19:38:35 -0700

    Add option to restore last used directory when switching back to folder view

--- app/src/main/java/code/name/monkey/retromusic/fragments/folder/FoldersFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/folder/FoldersFragment.kt
@@ -67,6 +67,8 @@ import code.name.monkey.retromusic.model.Song
 import code.name.monkey.retromusic.providers.BlacklistStore
 import code.name.monkey.retromusic.util.FileUtil
 import code.name.monkey.retromusic.util.PreferenceUtil.startDirectory
+import code.name.monkey.retromusic.util.PreferenceUtil.lastDirectory
+import code.name.monkey.retromusic.util.PreferenceUtil.saveLastDirectory
 import code.name.monkey.retromusic.util.ThemedFastScroller.create
 import code.name.monkey.retromusic.util.getExternalStorageDirectory
 import code.name.monkey.retromusic.util.getExternalStoragePublicDirectory
@@ -108,6 +110,7 @@ class FoldersFragment : AbsMainActivityFragment(R.layout.fragment_folder),
     }
     private var storageItems = ArrayList<Storage>()
     private val scanViewModel: ScanViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentFolderBinding.bind(view)
@@ -133,10 +136,15 @@ class FoldersFragment : AbsMainActivityFragment(R.layout.fragment_folder),
                 }
             })
         if (savedInstanceState == null) {
+            val restoreDir = if (saveLastDirectory) {
+                lastDirectory;
+            } else {
+                startDirectory;
+            }
             switchToFileAdapter()
             setCrumb(
                 Crumb(
-                    FileUtil.safeGetCanonicalFile(startDirectory)
+                    FileUtil.safeGetCanonicalFile( restoreDir )
                 ),
                 true
             )
@@ -203,6 +211,9 @@ class FoldersFragment : AbsMainActivityFragment(R.layout.fragment_folder),
     override fun onFileMenuClicked(file: File, view: View) {
         val popupMenu = PopupMenu(requireActivity(), view)
         if (file.isDirectory) {
+            if (saveLastDirectory) {
+                lastDirectory = file;
+            }
             popupMenu.inflate(R.menu.menu_item_directory)
             popupMenu.setOnMenuItemClickListener { item: MenuItem ->
                 when (val itemId = item.itemId) {
@@ -282,6 +293,9 @@ class FoldersFragment : AbsMainActivityFragment(R.layout.fragment_folder),
         var mFile = file
         mFile = tryGetCanonicalFile(mFile) // important as we compare the path value later
         if (mFile.isDirectory) {
+            if (saveLastDirectory) {
+                lastDirectory = mFile;
+            }
             setCrumb(Crumb(mFile), true)
         } else {
             val fileFilter = FileFilter { pathname: File ->
@@ -473,6 +487,9 @@ class FoldersFragment : AbsMainActivityFragment(R.layout.fragment_folder),
             return
         }
         val path = crumb.file.path
+        if (saveLastDirectory) {
+            lastDirectory = crumb.file;
+        }
         if (path == "/" || path == "/storage" || path == "/storage/emulated") {
             switchToStorageAdapter()
         } else {
